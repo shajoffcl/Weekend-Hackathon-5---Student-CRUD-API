ae85529789c3ad06cd20ff371b57abcb6cd07911
shajoffcl <shaj.native@gmail.com>
2020-11-15 17:24:37 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ const bodyParser = require("body-parser");
 const port = 8080
 app.use(express.urlencoded());
 
+const studentData=require("./InitialData");
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
+
+app.get('/api/student', (req, res)=>{
+    res.send(studentData);
+})
+
+app.get('/api/student/:id', (req, res)=>{
+    const id=req.params.id;
+    const student=studentData.find((student)=>student.id===parseInt(id));
+    if(!student){
+        res.sendStatus(404);
+        return;
+    }
+    res.send(student);
+})
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
